0edc16f08099affdb34bf53ae87a1d9653df09e3
kalupahanatd <67464246+kalupahanatd@users.noreply.github.com>
2021-01-15 19:50:38 +0530

    edited fruit page

--- src/components/Fruit.js
+++ src/components/Fruit.js
@@ -6,11 +6,16 @@ import './css/imgbox.css'
 import banana from'./../fruit/banana.jpg'
 import mango from './../fruit/mango.jpg'
 import pineapple from'./../fruit/pineapple.jpg'
-//mport avacado from './../fruit/avocados.jpg'
+import avacado from './../fruit/avocados.jpg'
 import blue_grapes from'./../fruit/blue grapes.jpg'
 import guawa from './../fruit/guava.jpg'
-//import papaya from'./../fruit/papaya.jpg'
-//import orange from './../fruit/orange.jpg'
+import papaya from'./../fruit/papaya.jpg'
+import orange from './../fruit/orange.jpg'
+import dragon_fruit from './../fruit/dragon_fruit.jpg'
+import Katu_anoda from './../fruit/Katu_Anoda.jpg'
+import Rambutan from './../fruit/rambutan.jpg'
+import Uguressa from './../fruit/uguressa.jpg'
+import strawberry from './../fruit/strawberry.jfif'
 
 const Fruit = () => {
     return(
@@ -60,6 +65,81 @@ const Fruit = () => {
                         </Link>
                     </div>
                 }
+                {
+                    <div className="minicard">
+                        <img src={avacado} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Avacado</button>
+                        </Link>
+                    </div>
+                }
+                {
+                    <div className="minicard">
+                        <img src={dragon_fruit} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Dragon fruit</button>
+                        </Link>
+                    </div>
+                }
+
+                {
+                    <div className="minicard">
+                        <img src={strawberry} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Strawberry</button>
+                        </Link>
+                    </div>
+                }
+
+                {
+                    <div className="minicard">
+                        <img src={Katu_anoda} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Katu anoda</button>
+                        </Link>
+                    </div>
+                }
+                {
+                    <div className="minicard">
+                        <img src={orange} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Orange</button>
+                        </Link>
+                    </div>
+                }
+                {
+                    <div className="minicard">
+                        <img src={pineapple} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Pineapple</button>
+                        </Link>
+                    </div>
+                }
+                {
+                    <div className="minicard">
+                        <img src={Uguressa} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Uguressa</button>
+                        </Link>
+                    </div>
+                }
+                {
+                    <div className="minicard">
+                        <img src={Rambutan} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Rambutan</button>
+                        </Link>
+                    </div>
+                }
+                {
+                    <div className="minicard">
+                        <img src={papaya} alt=""/>
+                        <Link to="./About">
+                            <button type="button">Papaya</button>
+                        </Link>
+                    </div>
+                }
+
 
             </div>
 
