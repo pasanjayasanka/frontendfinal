e4bd124a7b9bf8a878bf38ae95b58363755b9291
pasan <pasanjayasanka7@gmail.com>
2021-05-05 17:13:49 +0530

    edit preview component

--- src/components/Review/NewReview.js
+++ src/components/Review/NewReview.js
@@ -1,12 +1,40 @@
 import {Component} from "react";
-
+import {Form,Button} from "react-bootstrap";
+import {Input} from "reactstrap";
 
 class NewReview extends Component{
 
     render() {
         return(
-            <div>
-                <p>Review work!!!!</p>
+            <div className="tab">
+                <div>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Product :</th>
+                            <th>Orange</th>
+                        </tr>
+                        <tr>
+                            <th>Feedback Send as:</th>
+                            <th>Manusha</th>
+                        </tr>
+                        </thead>
+                    </table>
+                </div>
+                <div>
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>Enter your feedback about this product</Form.Label>
+                            <Input name="newreview"/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label>Give a rating to the product</Form.Label>
+                            <Input name="newreview"/>
+                        </Form.Group>
+                        <Button variant="primary">Send</Button>
+                    </Form>
+                </div>
+
             </div>
         )
     }
